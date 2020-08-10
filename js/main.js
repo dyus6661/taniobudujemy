048a644cf49dde5a55e477ffbd91fde01f3bb29e
Eduard <dyus6661@gmail.com>
2020-08-11 00:27:02 +0300

    Initial commin

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const navLi = document.querySelectorAll("#navigation li");
+
+navLi.forEach((el) => {
+  ["click", "touchstart"].forEach((event) => {
+    el.addEventListener(
+      event,
+      () => {
+        navLi.forEach((_el) => _el.classList.remove("active"));
+        el.classList.add("active");
+      },
+      true
+    );
+  });
+});
+
+new ML.Tabs(".tabs", {
+  tab_class: "tabs__tab",
+  nav_class: "tabs__nav",
+  nav_item_class: "tabs__nav-item",
+  active_class: "tabs__active",
+});
